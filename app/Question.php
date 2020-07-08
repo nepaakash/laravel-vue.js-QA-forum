7bb2632b508181b98ad19c66b607460984f80d58
unknown <aakashnepal77@gmail.com>
2020-07-08 21:28:28 +0545

    populate question and user using factoru and seed

--- app/Question.php
+++ app/Question.php
@@ -7,8 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     protected $fillable=['title','body'];
-    public funtion user(){
+    public function user(){
         return $this->belongsTo(User::class);
+    }
+
+    public function setTitleAttribute($value){
+        $this->attributes['title']=$value;
+        $this->attributes['slug']=str_slug($value);
     }
 
     

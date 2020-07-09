aadac580901efc0544ca12d48ecf6bb20313b0e1
unknown <aakashnepal77@gmail.com>
2020-07-09 09:58:47 +0545

    votes,answer counter

--- resources/views/question/view.blade.php
+++ resources/views/question/view.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
+@section('css')
+<style>
 
+   
+    </style>
+
+@endsection
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -10,6 +16,19 @@
                 <div class="card-body">
                    @foreach($question as $questions)
                    <div class="media">
+                   <div class="d-flex flex-column counters">
+                   <div class="vote">
+                        <strong>{{$questions->votes}}</strong>{{str_plural('vote',$questions->votes)}}
+                       </div>
+                   
+                   <div class="status {{$questions->status}}">
+                        <strong>{{$questions->answers}}</strong>{{str_plural('answer',$questions->answers)}}
+                       </div>
+                   
+                   <div class="views">
+                        {{$questions->views."".str_plural('view',$questions->views)}}
+                       </div>
+                   </div>
                     <div class="media-body">
                     <h3 class="mt-0"><a href="{{$questions->url}}">{{$questions->title}}</a></h3>
                     <p class="lead">Asked By <a href="{{$questions->user->url}}">{{$questions->user->name}}</a></p>
